482f8aea42c816669606413cce5f0b81a8b8db01
johnny lamb <agnusjack@gmail.com>
2015-08-31 22:46:00 -0600

    temp is in the console!

--- js/main.js
+++ js/main.js
@@ -1,42 +1,39 @@
-$(document).on('ready', function(event) {
+$(document).on('ready', function() {
   console.log('sanity check!');
-   // event.preventDefault();
-   getResults();
 
 
-// var my_city="Washington,USA";
-// var my_key="85bb4ac0b5e70ea54dad0f87a4b88";
-// var num_of_days=2;
-// // build URI:
-// var uri="http://free.worldweatheronline.com/feed/weather.ashx?q="+my_city+"&key="+my_key+"&format=json&no_of_days="+no_of_days+"&includeLocation=yes";
-// // uri-encode it to prevent errors :
-// uri=encodeURI(uri);
+  var my_city = "New" + "York";
+  var my_key = "85bb4ac0b5e70ea54dad0f87a4b88";
+  var num_of_days = 1;
 
+  console.log('getting here');
 
-function getResults() {
-    $.ajax({
-      url: "HTTP: http://api.worldweatheronline.com/free/v2/weather.ashx",
-      method:"GET",
+  var getResults = function() {
+    var request = $.ajax({
+      url: "http://api.worldweatheronline.com/free/v2/weather.ashx",
+      method: "GET",
       data: {
-        key:my_key,
-        q:my_city,
+        key: my_key,
+        q: "New" + "York",
+
         num_of_days: num_of_days,
-        format: "jsonp"
+        format: "json"
       },
-       dataType: "jsonp",
-      success: function(apiResponse) {
-        temperature = apiResponse.data.current_condition[0].temp_F;
-        author = apiResponse.quoteAuthor;
-        console.log(temperature);
+      dataType: "json",
 
-        $(".temperature results-area").html(quote);
-        if(author === ''){
-
-          author = "Anonymous";
-        }
-        $(".group").html("--" + author);
-      }
     });
-  }
+    request.done(function(apiResponse) {
+      console.log(apiResponse.data.current_condition[0].temp_F);
+      // temperature = apiResponse.data.current_condition[0].temp_F;
+      // console.log(temperature);
+
+      // $(".temperature results-area").html(quote);
+    });
+  };
+
+  $(".btn").click(function(event) {
+    event.preventDefault();
+    getResults();
+  });
 
 });
