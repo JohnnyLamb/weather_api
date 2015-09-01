d4de2e2d57ad725b3a1994e511b1515b2b507281
johnny lamb <agnusjack@gmail.com>
2015-09-01 10:32:45 -0600

    adding dynamic search for weather

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).on('ready', function() {
 
-  var my_city = "Denver";
-  var my_key = "85bb4ac0b5e70ea54dad0f87a4b88";
-  var num_of_days = 1;
+
+
+$("form").on("submit",function(event) {
+    event.preventDefault();
+
+    getResults();
+    // console.log(my_city);
+  });
 
   var getResults = function() {
+    var my_city = $("#search-term").val().trim();
+    var num_of_days = 1;
+    var my_key = "85bb4ac0b5e70ea54dad0f87a4b88";
     var request = $.ajax({
       url: "http://api.worldweatheronline.com/free/v2/weather.ashx",
       method: "GET",
@@ -17,14 +25,11 @@ $(document).on('ready', function() {
       dataType: "json",
     });
     request.done(function(apiResponse) {
-      temperature = apiResponse.data.current_condition[0].temp_F;
-      $(".temp").html(temperature + " degrees in Fahrenheit");
-      $(".Location").html(my_city);
+      console.log(apiResponse.data.current_condition[0].temp_F);
+      // temperature = apiResponse.data.current_condition[0].temp_F;
+      // $(".temp").html(temperature + " degrees in Fahrenheit in " + my_city);
+      // $(".Location").html(my_city);
     });
   };
-  $(".btn").click(function(event) {
-    event.preventDefault();
-    getResults();
-  });
 
 });
